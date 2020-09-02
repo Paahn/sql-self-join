fa0bafb83eac76338757e0ea414c01cc1ff90f2e
Paahn <pjhatzis@gmail.com>
2020-09-01 20:58:50 -0700

    added description of db and first query

---
+++ self-join.sql
@@ -0,0 +1,9 @@
+-- Edinburgh Buses
+-- This database consists of two tables: stops and routes
+
+-- stops(id, name)
+-- route(num, company, pos, stop)
+
+
+-- 1 How many stops are in the database?
+SELECT count(id) FROM stops;
