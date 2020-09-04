0e00b54f3b6d600cff6e0c2fc765698f673f0f4a
Paahn <pjhatzis@gmail.com>
2020-09-03 18:18:27 -0700

    added initial code for query 4

--- self-join.sql
+++ self-join.sql
@@ -23,3 +23,6 @@ WHERE num='4' AND company='LRT';
 -- London Road (149) or Craiglockhart (53). Run the query and notice 
 -- the two services that link these stops have a count of 2.
 -- Add a HAVING clause to restrict the output to these two routes.
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
