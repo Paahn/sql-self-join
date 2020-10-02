3c363e311d7ab2c45a7f7a6c46a5da8ac9b7252c
Paahn <pjhatzis@gmail.com>
2020-10-01 18:22:54 -0700

    added description for query 9

--- self-join.sql
+++ self-join.sql
@@ -67,3 +67,8 @@ FROM route a JOIN route b ON
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart' 
 AND stopb.name='Tollcross';
+
+-- 9. Give a distinct list of the stops which may be reached
+-- from 'Craiglockhart' by taking one bus, including 'Craiglockhart' 
+-- itself, offered by the LRT company. Include the company and bus
+-- no. of the relevant services.
