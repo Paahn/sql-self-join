aa32468147c9cf8162e7abebbbbd62bae510db33
Paahn <pjhatzis@gmail.com>
2020-10-01 18:41:52 -0700

    work in progress

--- self-join.sql
+++ self-join.sql
@@ -72,3 +72,11 @@ AND stopb.name='Tollcross';
 -- from 'Craiglockhart' by taking one bus, including 'Craiglockhart' 
 -- itself, offered by the LRT company. Include the company and bus
 -- no. of the relevant services.
+SELECT DISTINCT stopa.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopb.name='Craiglockhart'
+ORDER BY a.num ASC, stopa.name;
+-- ^^ not working yet
